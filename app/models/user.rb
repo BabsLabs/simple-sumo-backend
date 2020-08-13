d5fd15fa8c788be8ee5e29e53aad94a156a8e816
Brian Bower <brian@brianbower.com>
2020-08-13 11:05:56 -0600

    Update email validation to use build in email regex instead of making a custom one

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,5 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :email, uniqueness: true
 
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-
+  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
 end
