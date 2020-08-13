337401a7a0aedd77f6e94a8a4ab74c561bc3d52c
Brian Bower <brian@brianbower.com>
2020-08-13 11:05:18 -0600

    Add rescue for ActiveRecord::NotFound error and update status codes returned to provide properly coded responses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,19 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find(params[:id])
-    if @user
-      @serialized_user = UserSerializer.new(@user)
-      render json: { user: @serialized_user }
-    else
-      render json: { errors: ['user not found'] , status: 500 }
-    end
+    begin
+      @user = User.find(params[:id])
+      if @user
+        @serialized_user = UserSerializer.new(@user)
+        render json: { user: @serialized_user }
+      else
+        render  status: 404, json: { errors: ['user not found'] }
+      end
+    rescue ActiveRecord::RecordNotFound  
+      render  status: 404, json: { errors: ['user not found'] }
+    return
+end
+
   end
 
   def create
@@ -15,9 +21,9 @@ class UsersController < ApplicationController
     if @user.save
       login!
       @serialized_user = UserSerializer.new(@user)
-      render json: { status: :created, user: @serialized_user }
+      render status: 201, json: { user: @serialized_user }
     else
-      render json: { status: 500, errors: @user.errors.full_messages }
+      render status: 400, json: { errors: @user.errors.full_messages }
     end
   end
 
