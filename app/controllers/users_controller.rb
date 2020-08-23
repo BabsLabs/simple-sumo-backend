440d23afb0b2dd3398153ad3c4ae47ee31d2d339
Brian Bower <brianbower@Brians-MacBook-Pro.local>
2020-08-22 21:24:32 -0600

    Add UserNotifierMailer to send confirmation of registration email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,13 @@ class UsersController < ApplicationController
     if @user.save
       login!
       @serialized_user = UserSerializer.new(@user)
+       # `recipient` is the email address that should be receiving the message
+      recipient = @user.email
+      # `email_info` is the information that we want to include in the email message.
+      email_info = { user: @user
+                 }
+      UserNotifierMailer.inform_registration(email_info, recipient).deliver_now
+ 
       render status: 201, json: { user: @serialized_user }
     else
       render json: { errors: @user.errors.full_messages }
