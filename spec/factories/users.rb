0ccc7bcd12783a138d351adfa2fec2f0bc21448d
Brian Bower <brianbower@Brians-MacBook-Pro.local>
2020-08-24 13:12:28 -0600

    Add a User factory to generate users in specs

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+password = Faker::Internet.password
+
+FactoryBot.define do
+  factory :user do
+    username { Faker::Internet.username }
+    email  { Faker::Internet.email }
+    password { password }
+    password_confirmation { password }
+  end
+end
