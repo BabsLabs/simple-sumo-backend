034512e8f8bab8f20bbf5c4c186e4c84dd495f4c
Brian Bower <brianbower@Brians-MacBook-Pro.local>
2020-08-24 18:25:32 -0600

    Add spec for unit testing UserNotifierMailer mailer contents

--- spec/mailers/user_notifier_spec.rb
+++ spec/mailers/user_notifier_spec.rb
@@ -1,5 +1,27 @@
 require "rails_helper"
 
-RSpec.describe UserNotifierMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe UserNotifierMailer, type: :mailer do
+  it 'sends a mailer' do
+    text_email_fixture = file_fixture("inform_registration_fixture.text.erb").read
+    html_email_fixture = file_fixture("inform_registration_fixture.html.erb").read
+
+    user = create(:user, username: 'TestUsername')
+    email_info = { user: user }
+    email = UserNotifierMailer.inform_registration(email_info)
+
+    email.deliver_now
+
+    
+    expect(email.from).to eq(['info@babslabs.com'])
+    expect(email.to).to eq([user.email])
+    expect(email.subject).to eq("You have registered with Simple Sumo #{user.username}!")
+    
+    expect(email.parts.length).to eq(2)
+    
+    expect(email.parts[0].content_type.to_s).to eq('text/plain; charset=UTF-8')
+    expect(email.parts[0].body).to eq(text_email_fixture)
+
+    expect(email.parts[1].content_type.to_s).to eq('text/html; charset=UTF-8')
+    expect(email.parts[1].body).to eq(html_email_fixture)
+  end
 end
