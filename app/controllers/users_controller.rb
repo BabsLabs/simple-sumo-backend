cbebed3d591f62b05c75fcbb4774c35adac80256
Brian Bower <brianbower@Brians-MacBook-Pro.local>
2020-08-27 10:23:39 -0600

    Add confirm and activate email methods and remove unneeded instance variables

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,10 @@ class UsersController < ApplicationController
 
   def show
     begin
-      @user = User.find(params[:id])
-      if @user
-        @serialized_user = UserSerializer.new(@user)
-        render json: { user: @serialized_user }
+      user = User.find(params[:id])
+      if user
+        serialized_user = UserSerializer.new(user)
+        render json: { user: serialized_user }
       else
         render  status: 404, json: { errors: ['user not found'] }
       end
@@ -16,15 +16,29 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.create(user_params)
     if @user.save
       login!
       UserRegistrationEmailJob.perform_later(@user.id)
-      @serialized_user = UserSerializer.new(@user)
-      render status: 201, json: { user: @serialized_user }
+      serialized_user = UserSerializer.new(@user)
+      render status: 201, json: { user: serialized_user }
     else
       render json: { errors: @user.errors.full_messages }
     end
+  end
+
+  def confirm_email
+    user = User.find_by_confirm_token(params[:id])
+    if user
+      user.verify_user
+      redirect_to("http://babslabs-simple-sumo.herokuapp.com/login")
+    end
+  end
+
+  def email_activate
+    self.email_confirmed = true
+    self.confirm_token = nil
+    save!(:validate => false)
   end
 
   private
