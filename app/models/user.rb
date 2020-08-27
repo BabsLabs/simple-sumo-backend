41febca359de007cd022db48723cc78c9b4d51ea
Brian Bower <brianbower@Brians-MacBook-Pro.local>
2020-08-27 10:24:48 -0600

    Add verify_user and confirm_token methods to process user email verification

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_create :confirmation_token
+
   has_secure_password
 
   validates :username, presence: true
@@ -8,4 +10,17 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
 
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
+
+  def verify_user
+    self.email_confirmed = true
+    self.confirm_token = nil
+    save!(:validate => false)
+  end
+
+  private
+
+    def confirmation_token
+      self.confirm_token = SecureRandom.urlsafe_base64.to_s if self.confirm_token.blank?
+    end   
+
 end
