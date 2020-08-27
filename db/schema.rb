4e89ce0e29d665d941392d9e0bd142af1e2f5d57
Brian Bower <brianbower@Brians-MacBook-Pro.local>
2020-08-27 10:27:27 -0600

    Add user email_confirmed and confirm_token columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_004333) do
+ActiveRecord::Schema.define(version: 2020_08_26_192141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2020_08_12_004333) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "email_confirmed", default: false
+    t.string "confirm_token"
   end
 
 end
