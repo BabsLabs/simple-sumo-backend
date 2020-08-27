f4177a1aa69046a4e55b5e1ef70c730a0be0b741
Brian Bower <brianbower@Brians-MacBook-Pro.local>
2020-08-27 17:45:42 -0600

    Add spec to assure user can't confirm email twice

--- spec/requests/users/users_email_verification_spec.rb
+++ spec/requests/users/users_email_verification_spec.rb
@@ -1,20 +1,35 @@
 require 'rails_helper'
 
 describe 'User email verification endpoint' do
-    it 'a User can verify their email' do
-        user = create(:user)
+  it 'a User can verify their email' do
+    user = create(:user)
 
-        get "/users/#{user.confirm_token}/confirm_email"
+    get "/users/#{user.confirm_token}/confirm_email"
 
-        user.reload
+    user.reload
 
-        expect(user.confirm_token).to eq(nil)
-        expect(user.email_confirmed).to eq(true)
+    expect(user.email_confirmed).to eq(true)
 
-        expect(response.status).to eq(302)
-        expect(response).to redirect_to("http://babslabs-simple-sumo.herokuapp.com/login")
+    expect(response.status).to eq(302)
+    expect(response).to redirect_to("http://babslabs-simple-sumo.herokuapp.com/login")
+    
+    expected_response_body = "<html><body>You are being <a href=\"http://babslabs-simple-sumo.herokuapp.com/login\">redirected</a>.</body></html>"
+    expect(response.body).to eq(expected_response_body)
+end
 
-        expected_response_body = "<html><body>You are being <a href=\"http://babslabs-simple-sumo.herokuapp.com/login\">redirected</a>.</body></html>"
-        expect(response.body).to eq(expected_response_body)
-    end
+it 'a User cannot verify their email twice' do
+    user = create(:user)
+    
+    get "/users/#{user.confirm_token}/confirm_email"
+    
+    user.reload
+    
+    get "/users/#{user.confirm_token}/confirm_email"
+    
+    expect(response.status).to eq(302)
+    expect(response).to redirect_to("http://babslabs-simple-sumo.herokuapp.com/already_registered")
+
+    expected_response_body = "<html><body>You are being <a href=\"http://babslabs-simple-sumo.herokuapp.com/already_registered\">redirected</a>.</body></html>"
+    expect(response.body).to eq(expected_response_body)
+  end
 end
