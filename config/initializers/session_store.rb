ac82322b7f41e2eb582c4599314f64f2916701e7
Brian Bower <brianbower@Brians-MacBook-Pro.local>
2020-08-28 19:24:12 -0600

    Add same site to session store for chrome warning

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_simple-sumo-backend', domain: 'https://babslabs-simple-sumo-backend.herokuapp.com'
+  Rails.application.config.session_store(:cookie_store, key: '_simple-sumo-backend', domain: 'https://babslabs-simple-sumo-backend.herokuapp.com', same_site: :none)
 else
-  Rails.application.config.session_store :cookie_store, key: '_simple-sumo-backend' 
+  Rails.application.config.session_store :cookie_store, key: '_simple-sumo-backend'
 end
