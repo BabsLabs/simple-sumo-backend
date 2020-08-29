180241318117be09a84a414089d7aac6ad136e98
Brian Bower <brianbower@Brians-MacBook-Pro.local>
2020-08-28 19:28:48 -0600

    Remove same_site from session_store as it didn't fix chrome warning

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-  Rails.application.config.session_store(:cookie_store, key: '_simple-sumo-backend', domain: 'https://babslabs-simple-sumo-backend.herokuapp.com', same_site: :none)
+  Rails.application.config.session_store(:cookie_store, key: '_simple-sumo-backend', domain: 'https://babslabs-simple-sumo-backend.herokuapp.com')
 else
   Rails.application.config.session_store :cookie_store, key: '_simple-sumo-backend'
 end
